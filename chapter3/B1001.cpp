643dd4ac29fc8657b6d7c82c250d9c8be8be0853
lee <664275973@qq.com>
2018-04-12 20:11:05 +0800

    update

--- chapter3/B1001.cpp
+++ chapter3/B1001.cpp
@@ -2,3 +2,25 @@
 // Created by 李云飞 on 2018/4/12.
 //
 
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int step = 0;
+
+    scanf("%d", &n);
+
+    do {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = (3 * n + 1) / 2;
+        }
+        step ++;
+    } while (n == 1);
+
+    printf("%d", step);
+
+    return 0;
+}
