ae738f042b69e7fe007cba2939796c41f88db84e
lee <664275973@qq.com>
2018-04-24 17:09:59 +0800

    优化算法-查找元素-考试座位号

--- chapter3/SearchElement/B1041.cpp
+++ chapter3/SearchElement/B1041.cpp
@@ -2,7 +2,7 @@
  * @Author: lee
  * @Date:   2018-04-24T16:38:15+08:00
  * @Last modified by:   lee
- * @Last modified time: 2018-04-24T17:00:49+08:00
+ * @Last modified time: 2018-04-24T17:09:09+08:00
  */
 
 // 查找元素
@@ -12,32 +12,28 @@
 // 考生信息
 struct stuInfo{
     long long sno;
-    // 试机座位号
-    int tryNo;
     // 考试座位号
-    int ExamNo;
+    int examNo;
 } stu[1000];
 
 int main()
 {
-    int n, m, temp;
+    int n, m, temp, tryNo, examNo;
+    long long sno;
     scanf("%d", &n);
 
     for (int i = 0; i < n; i ++) {
-        scanf("%lld%d%d", &(stu[i].sno), &(stu[i].tryNo), &(stu[i].ExamNo));
+        scanf("%lld%d%d\n", &sno, &tryNo, &examNo);
+        // 以试机座位号作为数组的下标
+        stu[tryNo].sno = sno;
+        stu[tryNo].examNo = examNo;
     }
 
     scanf("%d", &m);
-
     for (int i = 0; i < m; i ++) {
         scanf("%d", &temp);
 
-        for (int j = 0; j < n; j ++) {
-            if (stu[j].tryNo == temp) {
-                printf("%lld %d\n", stu[j].sno, stu[j].ExamNo);
-                break;
-            }
-        }
+        printf("%lld %d\n", stu[temp].sno, stu[temp].examNo);
     }
 
     return 0;
