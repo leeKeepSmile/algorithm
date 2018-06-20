de7d0e8a4cb7e000779dbea1868a2dc640895cee
lee <664275973@qq.com>
2018-06-20 17:32:09 +0800

    简单数学-数列的片段和

---
+++ chapter5/SimpleMath/B1049.cpp
@@ -0,0 +1,27 @@
+/**
+ * @Author: lee
+ * @Date:   2018-06-20T17:16:05+08:00
+ * @Last modified by:   lee
+ * @Last modified time: 2018-06-20T17:30:52+08:00
+ */
+
+// 简单数学
+
+// 思路：第i个数出现的次数等于i * (n + 1 - i)
+
+#include <cstdio>
+
+int main()
+{
+    int n;
+    double v, ans = 0;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i ++) {
+        scanf("%lf", &v);
+        ans += v * i * (n + 1 - i);
+    }
+
+    printf("%.2f\n", ans);
+}
